6307def0f6b8a631558f252a49949c5289ec9e19
kumarharsh22 <kumarharsh2203@email.com>
2023-06-27 22:21:20 +0530

    intial3

---
+++ src/IfElse.java
@@ -0,0 +1,28 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numb1=25;
+		int numb2= 24;
+		
+		if(numb1>numb2) {
+			System.out.println("gretaer");
+		}
+		else if(numb1<numb2){
+			System.out.println("less");
+			}
+		else if(numb1==numb2){
+			System.out.println("equal");
+			}
+		else if(numb1<numb2){
+			System.out.println("less than");
+			}
+		else{
+			System.out.println("not equal");
+			}
+		}
+	}
+
+
